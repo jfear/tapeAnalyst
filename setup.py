e18d19951fa45603b8ef70e580dd2ceabe7b54b3
Justin Fear <justin.fear@nih.gov>
2015-12-04 10:38:56 -0500

    adds sequp.py mechanism

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from distutils.core import setup
+
+requirements = open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).readlines()
+setup(
+    name = 'tapeAnalyst',
+    version = '0.1',
+    install_requires = requirements,
+    author = 'Justin Fear',
+    author_email = 'fearjm@nih.gov',
+    description = 'A utility to analysze images from the Agilent Tapestation and generate a set of metrics.',
+    packages=['tapeAnalyst', 'tapeAnalyst.templates', 'bin'],
+    scripts = ['bin/analyzeTapeStationPng'],
+)
