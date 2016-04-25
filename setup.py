a8410a411dd3ab1ed6a69a456cd44a2321f8efdc
Justin Fear <justin.fear@nih.gov>
2016-04-25 14:28:42 -0400

    Adds pacakge data

--- setup.py
+++ setup.py
@@ -12,4 +12,5 @@ setup(
     description = 'A utility to analysze images from the Agilent Tapestation and generate a set of metrics.',
     packages=['tapeAnalyst', 'tapeAnalyst.templates', 'bin'],
     scripts = ['bin/analyzeTapeStationPng'],
+    packages_data={'tapeAnalyst': 'tapeAnalyst/templates/*'}
 )
